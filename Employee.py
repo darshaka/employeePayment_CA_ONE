4b2ecbbfb76baef0b0743b98bdafa8a58d483ed9
tharanga <darshakat@gmail.com>
2022-04-04 10:02:33 +0100

    Define employee and test class

---
+++ Employee.py
@@ -0,0 +1,11 @@
+class Employee:
+
+    def __init__(self, staffID, lastName, firstName, regHours, hourlyRate, oTMultiple, taxCredit, standardBand):
+        self.staffId = staffID
+        self.lastName = lastName
+        self.firstName = firstName
+        self.regHours = regHours
+        self.hourlyRate = hourlyRate
+        self.oTMultiple = oTMultiple
+        self.taxCredit = taxCredit
+        self.standardBand = standardBand
---
+++ EmployeeTest.py
@@ -0,0 +1,19 @@
+import unittest
+
+from Employee import Employee
+
+class EmployeeTest(unittest.TestCase):
+    def test_employee(self):
+        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
+        self.assertEqual('12345', employee.staffId)
+        self.assertEqual('Green', employee.lastName)
+        self.assertEqual('Joe', employee.firstName)
+        self.assertEqual(37, employee.regHours)
+        self.assertEqual(16, employee.hourlyRate)
+        self.assertEqual(1.5, employee.oTMultiple)
+        self.assertEqual(72, employee.taxCredit)
+        self.assertEqual(710, employee.standardBand)
+
+
+if __name__ == '__main__':
+    unittest.main()
