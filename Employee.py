b72206b4813344a2dacee413d9e353d15363160d
tharanga <darshakat@gmail.com>
2022-04-05 17:34:28 +0100

    Calculation method mark as private

--- Employee.py
+++ Employee.py
@@ -1,5 +1,6 @@
 class Employee:
 
+    #
     employInfo = {}
     taxRates = {'standardRate': 20, 'higherRate': 40, 'PRSI': 4}
 
@@ -18,94 +19,93 @@ class Employee:
         if(totalWorkHours < 0) :
             raise ValueError('Invalid workHours count')
 
-        self.employInfo['name'] = self.getName()
+        self.employInfo['name'] = self.__getName()
         self.employInfo['Date'] = date
         self.employInfo['Regular Hours Worked'] = self.regHours
-        self.employInfo['Overtime Hours Worked'] = self.getOverTimeHours(totalWorkHours)
+        self.employInfo['Overtime Hours Worked'] = self.__getOverTimeHours(totalWorkHours)
         self.employInfo['Regular Rate'] = self.hourlyRate
-        self.employInfo['Overtime Rate'] = self.getOverTimeRate()
-        self.employInfo['Regular Pay'] = self.getRegularPay(totalWorkHours)
-        self.employInfo['Overtime Pay'] = self.getOverTimePay(self.employInfo['Overtime Hours Worked'])
-        self.employInfo['Gross Pay'] = self.getGrossPay(self.employInfo['Regular Pay'], self.employInfo['Overtime Pay'])
-        self.employInfo['Standard Rate Pay'] = self.getStandardRatePay(self.standardBand, self.employInfo['Gross Pay'])
-        self.employInfo['Higher Rate Pay'] = self.getHigherRatePay(self.employInfo['Gross Pay'], self.employInfo['Standard Rate Pay'])
-        self.employInfo['Standard Tax'] = self.getStandardTax(self.employInfo['Standard Rate Pay'])
-        self.employInfo['Higher Tax'] = self.getHigherTax(self.employInfo['Higher Rate Pay'])
-        self.employInfo['Total Tax'] = self.getTotalTax(self.employInfo['Standard Tax'], self.employInfo['Higher Tax'])
+        self.employInfo['Overtime Rate'] = self.__getOverTimeRate()
+        self.employInfo['Regular Pay'] = self.__getRegularPay(totalWorkHours)
+        self.employInfo['Overtime Pay'] = self.__getOverTimePay(self.employInfo['Overtime Hours Worked'])
+        self.employInfo['Gross Pay'] = self.__getGrossPay(self.employInfo['Regular Pay'], self.employInfo['Overtime Pay'])
+        self.employInfo['Standard Rate Pay'] = self.__getStandardRatePay(self.standardBand, self.employInfo['Gross Pay'])
+        self.employInfo['Higher Rate Pay'] = self.__getHigherRatePay(self.employInfo['Gross Pay'], self.employInfo['Standard Rate Pay'])
+        self.employInfo['Standard Tax'] = self.__getStandardTax(self.employInfo['Standard Rate Pay'])
+        self.employInfo['Higher Tax'] = self.__getHigherTax(self.employInfo['Higher Rate Pay'])
+        self.employInfo['Total Tax'] = self.__getTotalTax(self.employInfo['Standard Tax'], self.employInfo['Higher Tax'])
         self.employInfo['Tax Credit'] = self.taxCredit
-        self.employInfo['Net Tax'] = self.getNetTax(self.employInfo['Total Tax'], self.employInfo['Tax Credit'])
-        self.employInfo['PRSI'] = self.getPRSI(self.employInfo['Gross Pay'])
-        self.employInfo['Net Deductions'] = self.getNetDeductions(self.employInfo['Net Tax'], self.employInfo['PRSI'])
-        self.employInfo['Net Pay'] = self.getNetPay(self.employInfo['Gross Pay'], self.employInfo['Net Deductions'])
+        self.employInfo['Net Tax'] = self.__getNetTax(self.employInfo['Total Tax'], self.employInfo['Tax Credit'])
+        self.employInfo['PRSI'] = self.__getPRSI(self.employInfo['Gross Pay'])
+        self.employInfo['Net Deductions'] = self.__getNetDeductions(self.employInfo['Net Tax'], self.employInfo['PRSI'])
+        self.employInfo['Net Pay'] = self.__getNetPay(self.employInfo['Gross Pay'], self.employInfo['Net Deductions'])
         return self.employInfo
 
 
-    def getName(self):
+    def __getName(self):
         return self.firstName + " " + self.lastName
 
 
-    def getOverTimeHours(self, totalWorkHours):
+    def __getOverTimeHours(self, totalWorkHours):
         if(self.regHours > totalWorkHours):
             return 0
         return totalWorkHours - self.regHours
 
 
-    def getOverTimeRate(self):
+    def __getOverTimeRate(self):
         return self.hourlyRate * self.oTMultiple
 
 
-    def getRegularPay(self, totalWorkHours):
+    def __getRegularPay(self, totalWorkHours):
         if(self.regHours > totalWorkHours):
             return totalWorkHours * self.hourlyRate
         return self.regHours * self.hourlyRate
 
+    def __getOverTimePay(self, overTimeHours):
+        return self.__getOverTimeRate() * overTimeHours
 
-    def getOverTimePay(self, overTimeHours):
-        return self.getOverTimeRate() * overTimeHours
 
-
-    def getGrossPay(self, regularPay, overTimePay):
+    def __getGrossPay(self, regularPay, overTimePay):
         return regularPay + overTimePay
 
 
-    def getHigherRatePay(self, grossPay, standardPay):
+    def __getHigherRatePay(self, grossPay, standardPay):
         if(grossPay < standardPay):
             return 0
         return grossPay - standardPay
 
 
-    def getStandardRatePay(self, standardBand, grossPay):
+    def __getStandardRatePay(self, standardBand, grossPay):
         if(grossPay > standardBand):
             return standardBand
         return grossPay
 
 
-    def getStandardTax(self, standardPay):
+    def __getStandardTax(self, standardPay):
         return standardPay * self.taxRates['standardRate'] / 100
 
 
-    def getHigherTax(self, higherPay):
+    def __getHigherTax(self, higherPay):
         return higherPay * self.taxRates['higherRate'] / 100
 
 
-    def getTotalTax(self, standardTax, higherTax):
+    def __getTotalTax(self, standardTax, higherTax):
         return standardTax + higherTax
 
 
-    def getNetTax(self, taxTotal, taxCredit):
+    def __getNetTax(self, taxTotal, taxCredit):
         netTax = float(f'{(taxTotal - taxCredit):g}')
         if(netTax < 0):
             return 0
         return netTax
 
 
-    def getPRSI(self, grossPay):
+    def __getPRSI(self, grossPay):
         return grossPay * self.taxRates['PRSI'] / 100
 
 
-    def getNetDeductions(self, netTax, pRSI):
+    def __getNetDeductions(self, netTax, pRSI):
         return netTax + pRSI
 
 
-    def getNetPay(self, grossPay, netDecution):
+    def __getNetPay(self, grossPay, netDecution):
         return grossPay - netDecution
--- EmployeeTest.py
+++ EmployeeTest.py
@@ -37,6 +37,10 @@ class EmployeeTest(unittest.TestCase):
         employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
         result = employee.computePayment(0, '31/10/2021')
         self.assertLessEqual(result['Net Pay'], result['Gross Pay'])
+        self.assertGreaterEqual(result['Overtime Hours Worked'], 0)
+        self.assertGreaterEqual(result['Overtime Pay'], 0)
+        self.assertGreaterEqual(result['Higher Tax'], 0)
+        self.assertGreaterEqual(result['Net Pay'], 0)
 
 
     def test_employee4(self):
@@ -46,83 +50,9 @@ class EmployeeTest(unittest.TestCase):
 
 
     def test_getName(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
+        employee = Employee('12345', 'One', 'Two', 37, 16, 1.5, 72, 710)
         result = employee.computePayment(42, '31/10/2021')
-        self.assertEqual('Joe Green', result['name'])
-
-
-    def test_validateOverTimeHours(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(5, employee.getOverTimeHours(42))
-        self.assertEqual(0, employee.getOverTimeHours(37))
-        self.assertEqual(0, employee.getOverTimeHours(20))
-
-
-    def test_calculateOverTimeRate(self):
-        employee1 = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(24, employee1.getOverTimeRate())
-
-        employee2 = Employee('12345', 'Green', 'Joe', 37, 21, 1.5, 72, 710)
-        self.assertEqual(31.5, employee2.getOverTimeRate())
-
-
-    def test_calculateRegularPay(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(592, employee.getRegularPay(42))
-        self.assertEqual(592, employee.getRegularPay(37))
-        self.assertEqual(432, employee.getRegularPay(27))
-
-
-    def test_calculateOverTimePay(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(120, employee.getOverTimePay(5))
-        self.assertEqual(0, employee.getOverTimePay(0))
-
-
-    def test_calculateGrossPay(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(20, employee.getGrossPay(15, 5))
-
-
-    def test_calculateHigherRatePay(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(10, employee.getHigherRatePay(720, 710))
-        self.assertEqual(0, employee.getHigherRatePay(700, 710))
-
-
-    def test_calculateStandardTax(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(100, employee.getStandardTax(500))
-
-
-    def test_calculateHigherTax(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(.8, employee.getHigherTax(2))
-
-
-    def test_calculateTotalTax(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(142.8, employee.getTotalTax(142, .8))
-
-
-    def test_calculateNetTax(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(float(f'{70.8:g}'), employee.getNetTax(142.8, 72))
-
-
-    def test_calculatePRSI(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(28.48, employee.getPRSI(712))
-
-
-    def test_netDeduction(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(99.28, employee.getNetDeductions(70.8, 28.48))
-
-
-    def test_calculateNetPay(self):
-        employee = Employee('12345', 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-        self.assertEqual(612.72, employee.getNetPay(712, 99.28))
+        self.assertEqual('Two One', result['name'])
 
 
 if __name__ == '__main__':
